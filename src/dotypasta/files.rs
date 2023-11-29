0b6d9121b5f798167280fcbccba20a30878be9ef
Théo BL <theo.biasutto-lervat@inria.fr>
2023-11-29 10:07:58 +0100

    :bug: create directories if needed instead of silent fail

--- src/dotypasta/files.rs
+++ src/dotypasta/files.rs
@@ -1,19 +1,30 @@
 use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::path::Path;
 
 pub fn dotypasta_to_host(config: HashMap<String, Vec<String>>) {
     config
         .iter()
         .flat_map(|(appname, filenames)| {
+            // TODO better UI
             println!("Applying {}", appname);
             filenames.iter()
         })
         .for_each(|filename| {
-            let home = env::var("HOME").unwrap() + "/";
-            let dotypasta = home.clone() + crate::dotypasta::REPO_PATH + "/";
-            println!("copy {}{} to {}{}", home, filename, dotypasta, filename);
-            let _ = fs::copy(dotypasta + filename, home + filename);
+            let dotypasta =
+                env::var("HOME").unwrap() + "/" + crate::dotypasta::REPO_PATH + "/" + filename;
+            let home = env::var("HOME").unwrap() + "/" + filename;
+            let src_path = Path::new(&dotypasta);
+            let dst_path = Path::new(&home);
+            // TODO better UI
+            println!("copy {:?} to {:?}", src_path, dst_path);
+            if let Some(dir) = dst_path.parent() {
+                if let Ok(false) = dir.try_exists() {
+                    let _ = fs::create_dir_all(dir);
+                }
+            }
+            let _ = fs::copy(src_path, dst_path);
         });
 }
 
@@ -21,13 +32,23 @@ pub fn host_to_dotypasta(config: HashMap<String, Vec<String>>) {
     config
         .iter()
         .flat_map(|(appname, filenames)| {
+            // TODO better UI
             println!("Saving {}", appname);
             filenames.iter()
         })
         .for_each(|filename| {
-            let home = env::var("HOME").unwrap() + "/";
-            let dotypasta = home.clone() + crate::dotypasta::REPO_PATH + "/";
-            println!("copy {}{} to {}{}", home, filename, dotypasta, filename);
-            let _ = fs::copy(home + filename, dotypasta + filename);
+            let dotypasta =
+                env::var("HOME").unwrap() + "/" + crate::dotypasta::REPO_PATH + "/" + filename;
+            let home = env::var("HOME").unwrap() + "/" + filename;
+            let src_path = Path::new(&home);
+            let dst_path = Path::new(&dotypasta);
+            // TODO better UI
+            println!("copy {:?} to {:?}", src_path, dst_path);
+            if let Some(dir) = dst_path.parent() {
+                if let Ok(false) = dir.try_exists() {
+                    let _ = fs::create_dir_all(dir);
+                }
+            }
+            let _ = fs::copy(src_path, dst_path);
         });
 }
